133582c6b35362bf0aa55ffacccc865393ee9084
Muhyiddin <muhyiddinmahfudz20@gmail.com>
2025-02-10 22:33:38 +0700

    CRUD Listing

---
+++ app/Models/Transaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+    protected $guarded=['id'];
+
+    public function setListingIdAttribute($value)
+    {
+        $listing = Listing::find($value);
+        $totalDays = Carbon::createFromDate($this->attributes['start_date'])->diffInDays($this->attributes['end_date']) + 1;
+        $totalPrice = $listing->price_per_day * $totalDays;
+        $fee = $totalPrice * 0.1;
+
+        $this->attributes['listing_id'] = $value;
+        $this->attributes['price_per_day'] = $listing->price_per_day;
+        $this->attributes['total_days'] = $totalDays;
+        $this->attributes['fee'] = $fee;
+        $this->attributes['total_price'] = $totalPrice + $fee;
+    }
+
+
+}
